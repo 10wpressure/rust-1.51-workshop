ce351eab5246a8d0231abc0f703b788affbce31f
Boris Kuznetsov <boris.kuznetsov@latoken.com>
2022-06-12 23:57:52 +0300

    all done

--- part3/src/main.rs
+++ part3/src/main.rs
@@ -2,6 +2,7 @@ enum CitySize {
     Town,       // approximate residents: 1_000
     City,       // approximate residents: 10_000
     Metropolis, // approximate residents: 1_000_000
+    Area{ residents: u64 }
 }
 
 struct City {
@@ -21,16 +22,29 @@ impl City {
                     residents,
                 )
             }
-            // 👉 TODO Handle the other CitySize variants individually,
-            //    in a similar way to how *town* is handled here
-            _ => {
-                let residents = 1_000;
+
+            CitySize::City => {
+                let residents = 10_000;
 
                 (
-                    format!(
-                        "an *unknown-size city* of approximately {} residents",
-                        residents
-                    ),
+                    format!("a *city* of approximately {} residents", residents),
+                    residents,
+                )
+            }
+
+
+            CitySize::Metropolis => {
+                let residents = 1_000_000;
+
+                (
+                    format!("a *metropolis* of approximately {} residents", residents),
+                    residents,
+                )
+            }
+
+            CitySize::Area{residents} => {
+                (
+                    format!("an *area* of approximately {} residents", residents),
                     residents,
                 )
             }
@@ -46,10 +60,7 @@ impl City {
 
 fn main() {
     // 👉 TODO Use City::new() to create a Metropolis-sized city here
-    let rustville = City {
-        description: String::new(),
-        residents: 0,
-    };
+    let rustville = City::new(CitySize::Area{residents:10}, true);
 
     println!("This city is {}", rustville.description);
 
